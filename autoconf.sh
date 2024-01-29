a504319fdad5682bcf764df0fd930b5bf972c2cf
Wasabi Burns <69662763+spiceywasabi@users.noreply.github.com>
2024-01-29 11:22:42 -0800

    Update autoconf.sh

--- autoconf.sh
+++ autoconf.sh
@@ -1,7 +1,53 @@
 #!/bin/vbash
+
+function get_next_if() {
+        excluded_interfaces=("$@")
+        lowest_ifindex=-1
+        lowest_ifname=""
+        for interface_path in /sys/class/net/*; do
+                found=false
+                interface=$(basename "$interface_path")
+                for excluded_interface in "${excluded_interfaces[@]}"; do
+                        if [ "$excluded_interface" == "$interface" ]; then
+                                found=true
+                                break
+                        fi
+                done
+                if [ "$found" == true ]; then
+                        continue
+                fi
+                ifindex=$(udevadm info -q property "$interface_path" | grep -E '^IFINDEX=' | cut -d'=' -f2)
+                if [[ "$ifindex" =~ ^[0-9]+$ ]]; then
+                        if [ "$lowest_ifindex" -eq -1 ] || [ "$ifindex" -lt "$lowest_ifindex" ]; then
+                          lowest_ifindex="$ifindex"
+                          lowest_ifname="$interface"
+                        fi
+                fi
+        done
+        if [ "$lowest_ifindex" -eq -1 ]; then
+                echo ""
+        else
+                echo "$lowest_ifname"
+        fi
+}
+
+echo "Determining Interfaces"
+
+# workaround for udevd, not ideal but workable. 
+WAN_INT=$(get_next_if "lo")
+LAN_INT=$(get_next_if "lo" "${WAN_INT}")
+
+if [[ -n "$WAN_INT" && -n "$LAN_INT" && "$WAN_INT" != "$LAN_INT" ]]; then
+    echo "Interfaces are good: WAN:${WAN_INT}, LAN:${LAN_INT}"
+else
+    echo "Conditions not met. Interfaces not correct. WAN:${WAN_INT}, LAN:${LAN_INT}"
+    exit 1
+fi
+
 echo "Beginning Configuration of Router NAT rules"
-export IPV4_ADDR=$(ip -4 addr show dev eth0 | grep -oP 'inet\s+\K[^\s]+'|sed "s/\([0-9]\+\.[0-9]\+\.[0-9]\+\.\)\([0-9]\+\)\/\([0-9]\+\)/\10\/\3/")
-export CURRENT_LAN_ADDR=$(ip -4 addr show dev eth1 | grep -oP 'inet\s+\K[^\s]+')
+export IPV4_ADDR=$(ip -4 addr show dev $WAN_INT | grep -oP 'inet\s+\K[^\s]+'|sed "s/\([0-9]\+\.[0-9]\+\.[0-9]\+\.\)\([0-9]\+\)\/\([0-9]\+\)/\10\/\3/")
+export TEAM_ROUTER=$(echo "${IPV4_ADDR}" | awk -F'.' '{print $3}')
+export CURRENT_LAN_ADDR=$(ip -4 addr show dev $LAN_INT | grep -oP 'inet\s+\K[^\s]+')
 
 export FINAL_LAN_ADDR="192.168.220.2/24"
 export FINAL_LAN_SUBNET=$(echo "${FINAL_LAN_ADDR}"|sed "s/\([0-9]\+\.[0-9]\+\.[0-9]\+\.\)\([0-9]\+\)\/\([0-9]\+\)/\10\/\3/")
@@ -31,31 +77,32 @@ configure
 echo "deleting base configurations"
 delete system login banner
 delete nat
-delete interfaces ethernet eth1
+delete interfaces ethernet $LAN_INT
 delete service lldp
 delete service ntp
 delete service dns forwarding
 commit
-echo "configuring interface eth1"
-set interfaces ethernet eth1 address '192.168.220.2/24'
+echo "configuring interface $LAN_INT"
+set interfaces ethernet $LAN_INT address '192.168.220.2/24'
 commit
 echo "setting nat rules"
 set nat destination rule 10 destination address '$FINAL_LAN_SUBNET'
-set nat destination rule 10 inbound-interface 'eth1'
+set nat destination rule 10 inbound-interface '$LAN_INT'
 set nat destination rule 10 translation address '$IPV4_ADDR'
 set nat destination rule 20 destination address '$IPV4_ADDR'
-set nat destination rule 20 inbound-interface 'eth0'
+set nat destination rule 20 inbound-interface '$WAN_INT'
 set nat destination rule 20 translation address '$FINAL_LAN_SUBNET'
-set nat source rule 10 outbound-interface 'eth0'
+set nat source rule 10 outbound-interface '$WAN_INT'
 set nat source rule 10 source address '$IPV4_ADDR'
 set nat source rule 10 translation address '$FINAL_LAN_SUBNET'
-set nat source rule 20 outbound-interface 'eth1'
+set nat source rule 20 outbound-interface '$LAN_INT'
 set nat source rule 20 source address '$FINAL_LAN_SUBNET'
 set nat source rule 20 translation address '$IPV4_ADDR'
-set nat source rule 30 outbound-interface 'eth0'
+set nat source rule 30 outbound-interface '$WAN_INT'
 set nat source rule 30 translation address 'masquerade'
 commit
 echo "setting services"
+set system host-name 'team-router$TEAM_ROUTER'
 set system time-zone America/Los_Angeles
 set firewall all-ping enable
 set service lldp interface all
